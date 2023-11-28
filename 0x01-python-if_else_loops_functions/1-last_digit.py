f91edd0fb4852fcd4fb32e0fbb2bb96856f05acf
Torubiri Walter <toruwalt1997@gmail.com>
2023-11-28 04:33:57 -0800

    Python - IELF Task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+actual_value = abs(number)
+last_digit = actual_value % 10
+if last_digit > 5:
+    x = "and is greater than 5"
+elif last_digit == 0:
+    x = "and is 0"
+elif last_digit <= 5:
+    x = "and is less than 6 and not 0"
+print(f"The Last digit of {number:d} is {last_digit:d} {x}")
