fa7ef46670bf900be45145708e1a048191315758
Torubiri Walter <toruwalt1997@gmail.com>
2023-11-28 12:59:09 -0800

    Python - IELF Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if ord(letter) > 96 and ord(letter) < 122:
+            letter = ord(letter) - 32
+        else:
+            letter = ord(letter)
+        print("{}".format(chr(letter)), end="")
+    print("")
