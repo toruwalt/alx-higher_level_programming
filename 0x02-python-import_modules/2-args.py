83fe68f94de3a9c6381bcbdf661ec7c2c7b0ba42
Torubiri Walter <toruwalt1997@gmail.com>
2023-11-30 21:10:55 +0100

    Python - Module Task 2 updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,13 @@
 if __name__ == "__main__":
     import sys
 
-    n_args = len(sys.argv)
-    print("{} arguments".format(n_args - 1))
-    for x in range(1, n_args):
+    n_args = len(sys.argv) - 1
+    if n_args == 0:
+        print("{} arguments.".format(n_args))
+    elif n_args == 1:
+        print("{} argument:".format(n_args))
+    else:
+        print("{} arguments:".format(n_args))
+
+    for x in range(1, n_args + 1):
         print("{}: {}".format(x, sys.argv[x]))
