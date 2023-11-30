24725df76d929feeeca6ac8370090b8e29fd5d23
Torubiri Walter <toruwalt1997@gmail.com>
2023-11-29 18:08:39 -0800

    Python -IELF Task 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            x = "FizzBuzz"
+        elif x % 3 == 0:
+            x = "Fizz"
+        elif x % 5 == 0:
+            x = "Buzz"
+        print("{}".format(x), end=" ")
