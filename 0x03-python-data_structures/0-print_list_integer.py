a7804f309bf3d684e4fcddad99ab1a3561b2a1fa
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-03 17:55:33 +0100

    Python - List and Tuples Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-"""Write a function that prints all integers of a list."""
-
 def print_list_integer(my_list=[]):
     for x in my_list:
-        print(x)
+        print("{:d}".format(my_list[i]))
