7ec6ef98ae25554da727b12cad60e3b0e6bc469c
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-03 12:28:44 -0800

    Python - List and Tuples Task 0

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > (len(my_list)):
+        return None
+    else:
+        for x in range(len(my_list)):
+            return(my_list[x - 2])
