64b93f7fbe4e4713b36f809a5e677bf9e27b5449
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-04 06:35:40 -0800

    Python - List and Tuple Task 3 checked if list existed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list = reversed(my_list)
-    for x in new_list:
-        print("{}".format(x))
+    if my_list:
+        new_list = reversed(my_list)
+        for x in new_list:
+            print("{}".format(x))
