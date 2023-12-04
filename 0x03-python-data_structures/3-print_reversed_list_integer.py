0bc885f53dd87ac5e1868befb72bbd9a833a9e71
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-04 06:40:21 -0800

    Python - List and Tuple Task 3 used reverse() instead of reversed(), added forloop with range

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         my_list.reverse()
-        for x in my_list:
-            print("{}".format(x))
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
