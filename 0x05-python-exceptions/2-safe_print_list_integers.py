025099a4f81e8881a35b134f47b06bf9ba6b4832
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-18 20:40:35 +0100

    Python - Exception Task 2: Changed the 'pass' prompt in the except clause to 'continue'

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print(int(my_list[i]), end="")
             count += 1
         except (TypeError, ValueError):
-            pass
+            continue
 
     print()
     return count
