366b9be531ca8699e49a4e85645fbb12d50117e8
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-18 15:47:41 +0000

    Python - Exceptions Task 3 made adjustment to the return format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-        return result
     except (ZeroDivisionError, TypeError, OverflowError, ValueError, FloatingPointError):
         result = None
     finally:
-        print('Inside result: ', result)
+        print(f'Inside result: {result}')
+    return result
