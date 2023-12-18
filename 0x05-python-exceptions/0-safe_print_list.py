92732a6a1aca2d35f2d0b5cf9c9da89795bed469
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-18 11:16:18 +0000

    Corrected the CodeStyles of Task 0 and 2 files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,5 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
 
-    print() 
+    print()
     return count
-
-
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
 
-    print() 
+    print()
     return count
-
-
