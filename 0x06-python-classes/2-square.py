2bf700ade5c0bd99093adaa66d2b9badaddbfab9
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-24 22:01:43 +0100

    Python - C & O Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Define class called Square"""
+
+
+class Square:
+
+    """Has a private instance attribute called size"""
+
+    def __init__(self, size=0):
+
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
