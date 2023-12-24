92d72c0b4827b4ac39a2f170a76e9916b1fade04
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-24 21:40:45 +0100

    Python - C & O Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a class called Square"""
+
+class Square:
+
+    """The class has a private instance attribute of size"""
+
+    def __init__(self,size):
+        self.__size = size
