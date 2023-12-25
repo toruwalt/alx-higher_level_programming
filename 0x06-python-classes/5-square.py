9e93639d1ed97e116125c586a4c0b9febed536b2
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-25 23:47:40 +0000

    Python - C & O Task 5:Added Exception Handling to Init

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,7 +22,12 @@ class Square:
 
     """The initialization at instantiation of the Square Class"""
     def __init__(self, size=0):
-        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     """Area of square of size passed"""
     def area(self):
