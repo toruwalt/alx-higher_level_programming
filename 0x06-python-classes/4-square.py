13d9fe7e01f0c236a3d54ded94f3d09fdf2eff41
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-25 22:22:38 +0000

    Python - C & O Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,16 +5,23 @@
 
 class Square:
 
-    """It has a private instance attribute of size"""
+    """A property(method turned attribute) of sizealso functions as a getter"""
+    @property
+    def size(self):
+        return self.__size
 
-    def __init__(self, size=0):
-
-        if (type(size) != int):
+    """A size attribute setter that checks if value is an integer and is greater than 0, then sets the size to the value"""
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
             raise TypeError("size must be an integer")
-        elif (size < 0):
-            raise valueError("size must be >= 0")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
+
+    """It has a private instance attribute of size"""
+    def __init__(self, size=0):                           self.__size = size
 
     """Public Instance method which returns the current square area"""
     def area(self):
