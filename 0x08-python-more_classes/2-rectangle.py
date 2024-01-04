dedaee51692617c29e21bade26ff62ef63cf7f78
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-04 14:17:33 +0100

    Python - More Classes Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""A class called Rectangle"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle.
+
+        Args:
+            width: The width of the rectangle
+            height: The height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates and return the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Calculates and return the perimeter of the rectangle"""
+        return (2 * (self.__width + self.__height))
