7edaf9f326d3f4047597a81d75ebe566ee1531b2
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-04 13:59:57 +0100

    Python - More Classes Task 1:Corrections to documentation 4

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
