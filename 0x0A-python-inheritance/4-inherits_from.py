d4fb88da8425d7781a832b2decdf70f0af7bf6fe
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-08 20:17:56 +0100

    Python - Inheritance Task 4: Incomplete

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Checks for inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True or False"""
+    x = type(obj)
+    y = type(a_class)
+    if issubclass(x, y):
+        return type(obj).__name__
