ed9cf1946ad9a63cea5d07262fd5cf53fbdd3b4e
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-09 10:31:18 +0000

    Python - IO Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that reads text"""
+
+
+def read_file(filename=""):
+    """Takes a read mode and prints the text"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
