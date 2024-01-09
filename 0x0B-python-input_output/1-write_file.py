6d363940d35f669599279ebbb39a895bd4da86c7
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-09 11:17:25 +0000

    Python - IO Task 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that writes to file"""
 
+
 def write_file(filename="", text=""):
     """Takes 2 parameters"""
     with open(filename, "w", encoding="utf-8") as f:
