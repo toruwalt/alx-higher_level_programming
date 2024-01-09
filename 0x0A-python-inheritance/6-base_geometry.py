a17c1967508913caaeaef231e8bbd04b73031ba2
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-09 01:01:12 +0000

    Python - Inheritance Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class that is empty"""
+
+    def area(self):
+        """Calculates and returns the area"""
+        raise Exception("area() is not implemented")
