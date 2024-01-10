5d5cbc827f749785faa1078ceb1e544e7ca7eff3
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-10 03:03:34 +0100

    Python - IO Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""Returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean)
-for JSON serialization of an object
-"""
+"""Returns the dict description with JSON serialization of an object"""
 import json
 
 
