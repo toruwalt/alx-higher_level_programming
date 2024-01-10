b1037c84da753451a8467cdeb9e64ec9844f5bcf
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-10 22:15:29 +0100

    Python - IO Task 11:To be done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Returns a dict representation of students"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Retrieves a dictionary representation of a Student instance"""
+    def to_json(self, attrs=None):
+        if attrs is None:
+            attrs = ["first_name", "last_name", "age"]
+        elif type(attrs) is not list:
+            raise TypeError("attrs must be a list of strings")
+        elif all(type(ele) is str for ele in attrs):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+    
+    """Replaces all attributes of the Student instance"""
+    def reload_from_json(self, json):
