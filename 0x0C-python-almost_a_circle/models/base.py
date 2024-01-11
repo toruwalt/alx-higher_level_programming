3320dcc2b15641380733de7f484ed68dd00128f0
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-11 20:36:21 +0000

    Python - AAC Task 0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Manage id attribute in all your future/
+classes and to avoid duplicating the same code"""
+
+
+class Base:
+    """The 'Base' of all other classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
