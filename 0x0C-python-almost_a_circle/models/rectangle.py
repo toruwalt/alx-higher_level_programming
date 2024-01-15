c5d645d4e766342378eb81b1eb44ea934a187a5f
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-15 13:07:45 +0100

    Python - AAC Task 8, I Skipped Task 7 for now

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,26 @@ class Rectangle(Base):
         else:
             print("")
 
+    def update(self, *args):
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.id = args[0]
+            self.__width = args[1]
+        if len(args) == 3:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+        if len(args) == 4:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+        if len(args) == 5:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
+        else:
+            pass
