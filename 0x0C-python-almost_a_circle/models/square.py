a7f8418084dc49739f6c99e65291b40623fe3c4a
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-16 22:18:46 +0100

    Python - AAC Task 10 Correction

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,23 +6,9 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Inheirts from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Ininitializes"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Returns string representation"""
         return f"[Square] ({self.id}) {self.x}/{self.y} {self.width}"
-"""
-    @property
-    def size(self):
-        #Defines the getter and setter of the size property
-        return self.__width
-
-    @size.setter
-    def size(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-        self.__height = value
-"""
