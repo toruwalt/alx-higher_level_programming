ff8510b856376316f1752bcd9e53dd0090ca4ec6
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-16 17:15:16 +0100

    While working on part 2 of Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,18 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] {self.id} {self.x}/{self.y} {self.size})"
+
+    @property
+    def size(self):
+        """Defines the getter and setter of the size property"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+        self.__height = value
+
