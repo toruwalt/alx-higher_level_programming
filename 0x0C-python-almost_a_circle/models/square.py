0e3b05c6b5eefad4dc51c89dee0bfd838b6bdd89
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-16 22:15:11 +0100

    Python - AAC Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,18 +6,15 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Inheirts from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
-        self.size = size
-        self.x = x
-        self.y = y
-        self.id = None
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] {self.id} {self.x}/{self.y} {self.size})"
-
+        """Returns string representation"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} {self.width}"
+"""
     @property
     def size(self):
-        """Defines the getter and setter of the size property"""
+        #Defines the getter and setter of the size property
         return self.__width
 
     @size.setter
@@ -28,4 +25,4 @@ class Square(Rectangle):
             raise ValueError("width must be >= 0")
         self.__width = value
         self.__height = value
-
+"""
