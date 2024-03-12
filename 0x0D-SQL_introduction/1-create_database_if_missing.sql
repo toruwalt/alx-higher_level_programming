de5c1c871a6103df57f1183fdcba975f786c1143
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-12 15:48:13 +0000

    SQL Intro - Task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
 -- creates the database hbtn_0c_0 in MySQL server.
+-- If the database hbtn_0c_0 already exists, script should not fail.
+-- Not allowed to use the SELECT or SHOW statements.
 CREATE DATABASE hbtn_0c_0;
