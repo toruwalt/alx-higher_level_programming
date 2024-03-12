a0637956d4c00a9e06f530df72204d6c021b1c32
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-12 16:15:43 +0000

    SQL Intro - Task 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -2,4 +2,4 @@
 -- If the database hbtn_0c_0 doesn’t exist, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements.
 
-DROP hbtn_0c_0 IF EXISTS;
+DROP IF EXISTS hbtn_0c_0;
