04f0cdfdfb439dfd7a1629df7b45c1da292afe69
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 01:59:12 +0000

    SQL Intro - Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,9 +7,11 @@
 -- If the table second_table already exists, your script should not fail
 -- You are not allowed to use the SELECT and SHOW statements
 
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) 
-VALUES (1, “John”, 10),
-(2, “Alex”, 3),
-(3, “Bob”, 14),
-(4, “George”, 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
