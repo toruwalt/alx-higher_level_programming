41d231c410596a6d7e14a00c3bf0150c3865409b
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 23:20:53 +0000

    SQL More - Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- states description:
+--	id INT unique, auto gen, not null and is a primary key
+--	name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
