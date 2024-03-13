9a8a6f3b933f85ccbf80ca5d63e5a1e2c81de9f8
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 22:44:25 +0000

    SQL More - Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 -- id_not_null description:
 --	id INT with the default value 1
 --	name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
