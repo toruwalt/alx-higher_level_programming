3cf24f8924e2e6a29d5ef71cdf296f3029ea7156
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 22:42:07 +0000

    SQL More - Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates the table id_not_null on your MySQL server.
--- force_name description:
---	id INT
---	name VARCHAR(256) can’t be null
+-- id_not_null description:
+--	id INT with the default value 1
+--	name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, VARCHAR(256));
