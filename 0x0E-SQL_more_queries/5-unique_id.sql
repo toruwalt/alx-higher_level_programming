6d115b74f49c55dce06d3f3825c5a2d58f3b8464
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 22:47:43 +0000

    SQL More - Task 5

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- creates the table unique_id on your MySQL server.
+-- unique_id description:
+--	id INT with the default value 1, is unique
+--	name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
