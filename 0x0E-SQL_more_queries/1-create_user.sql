0f09a96039dba9899e2cb406a29370f305dea3c2
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-13 22:01:31 +0000

    SQL More - Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'Googleman';
-GRANT ALL 
+GRANT ALL PRIVILEGES
 ON *.*
 TO 'user_0d_1'@'localhost';
