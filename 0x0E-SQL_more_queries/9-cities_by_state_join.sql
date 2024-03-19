bc242419ecfde21c04b31ef1942000ae0fe95b42
Torubiri Walter <toruwalt1997@gmail.com>
2024-03-19 16:27:08 +0000

    SQL More - Task 9 Redone

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,6 +4,6 @@
 -- only one SELECT statement
 SELECT *
 FROM cities
-FULL JOIN state
-ON cities.state_id = states.id
-WHERE cities.state_id IS NULL OR states.id IS NULL
+RIGHT JOIN states
+ON state.id = cities.states_id
+ORDER BY cities.id ASC
