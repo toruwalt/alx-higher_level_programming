791a23523298f286f82e35acc974de36ad5c3fe1
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-08 15:52:26 +0100

    Javascript WarmUp - Task 1:Redo

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++
@@ -1,5 +0,0 @@
--- lists the cities of California that can be found in the database hbtn_0d_usa.
--- The states table contains only one record where name = California
--- Results must be sorted in ascending order by cities.id
--- You are not allowed to use the JOIN keyword
-SELECT * FROM hbtn_0d_usa WHERE state = "California" ORDER BY cities.id ASC;
