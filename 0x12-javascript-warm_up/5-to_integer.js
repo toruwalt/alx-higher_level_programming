765631e40540d2bf6fdee94fb0428b04c7f5de1b
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-08 19:38:01 +0000

    Transfer to PC

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const numb = process.argv[2];
+console.log(typeof numb)
+
+/*
+if (typeof numb === 'number') {
+	const x = 'My number: ' + process.argv[2];
+	console.log(x);
+} else {
+	console.log(Number(numb))
+	console.log('Not a number');
+}
+*/
