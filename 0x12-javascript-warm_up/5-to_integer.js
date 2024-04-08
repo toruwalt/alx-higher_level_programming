f2d4a7d85334dd5f8353b11b34c921681b2e8d27
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-08 21:59:56 +0100

    Javascript WarmUp - Task 5: Corrected

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+const xxx = process.argv[2];
 const numb = Number(process.argv[2]);
 if (!isNaN(numb)) {
-  console.log('My number: ', numb);
+  console.log('My number: ', xxx);
 } else {
   console.log('Not a number');
 }
