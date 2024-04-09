6761fd31af255b4dd1c5f1fca19eca82739430e4
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-09 19:39:11 +0000

    Javasript OSC - Task 5 and Termix_to_PC Transfer

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Square extends Rectangle {
+  constructor(w) {
+    super(w, w);
+      this.width = w;
+  }
+
+  charPrint() {
+    if 
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.width; j++) {
+          const x = 'C';
+          process.stdout.write(x);
+	}
+	process.stdout.write('\n');
+      }
+    }
+  }
+
+    super.print()
+
+
+module.exports = Square;
