4f5219a1ff5a5f07c3394949dff62d05f7df86ed
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-09 21:19:55 +0100

    Javascript OSC - Task 6:Redo

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,17 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
 
-  charPrint(...args) {
-    if (args[0] === "C") {
+  charPrint (...args) {
+    if (args[0] === 'C') {
       for (let i = 0; i < this.height; i++) {
         for (let j = 0; j < this.width; j++) {
           const x = 'C';
           process.stdout.write(x);
-	}
+        }
         process.stdout.write('\n');
       }
     } else {
