f8ea23331ddb4be8ba31c76b7653a53dc211be95
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-09 23:11:10 +0100

    Javascript OSC - Task 7:Check

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (x of list) {
-    if (x === searchElement) {
-	    count++;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
     }
   }
   return count;
-}
+};
