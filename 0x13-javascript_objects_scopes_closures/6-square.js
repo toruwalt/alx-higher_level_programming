fb75930c7e2b5b1a969258b95c8e0389733596eb
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-09 21:18:50 +0100

    Javascript OSC - Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,6 +6,20 @@ class Square extends Rectangle {
   constructor(size) {
     super(size, size);
   }
+
+  charPrint(...args) {
+    if (args[0] === "C") {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          const x = 'C';
+          process.stdout.write(x);
+	}
+        process.stdout.write('\n');
+      }
+    } else {
+      super.print();
+    }
+  }
 }
 
 module.exports = Square;
