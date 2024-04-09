f59bbb24c5a9385c6f7050ee334bc9df168eb810
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-09 23:20:11 +0100

    Revert "If I don't edit, then it worked"
    This reverts commit f8ea23331ddb4be8ba31c76b7653a53dc211be95.

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      count++;
+  for (x of list) {
+    if (x === searchElement) {
+	    count++;
     }
   }
   return count;
-};
+}
