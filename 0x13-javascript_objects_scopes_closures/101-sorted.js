88275bcf31b1bf2553e2f3b2a6e356fc53990d06
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-10 00:27:01 +0100

    Javascript OSC - Task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const d = {};
+
+for (const k in dict) {
+  if (!(dict[k] in d)) {
+    d[dict[k]] = [k];
+  } else {
+    d[dict[k]].push(k);
+  }
+}
+
+console.log(d);
