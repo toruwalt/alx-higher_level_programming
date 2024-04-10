22b2056cca5b22a42e0dd148684c3f9942f72227
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-10 23:28:38 +0000

    Javasript WarmUp - Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const argA = process.argv[2];
+const a = +argA;
+
+function factorial(a) {
+  if (isNaN(a) || a === undefined) {
+    console.log(1);
+  } else {
+      if (a === 1 || a === 0) {
+        return 1;
+      }
+      else {
+        return a * factorial(a-1);
+      }
+  }
+}
+
+console.log(factorial(a));
