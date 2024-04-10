1acfae2c2f6c194daadf455a0f5022b243ae5767
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-10 23:29:57 +0000

    Javasript WarmUp - Task 10:SemiStandard fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,16 +3,15 @@
 const argA = process.argv[2];
 const a = +argA;
 
-function factorial(a) {
+function factorial (a) {
   if (isNaN(a) || a === undefined) {
     console.log(1);
   } else {
-      if (a === 1 || a === 0) {
-        return 1;
-      }
-      else {
-        return a * factorial(a-1);
-      }
+    if (a === 1 || a === 0) {
+      return 1;
+    } else {
+      return a * factorial(a - 1);
+    }
   }
 }
 
