c63054bfe2d1c862bc878671a6ec287975833964
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-26 17:45:20 +0100

    Python Network - Correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Python script that fetches status"""
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
     as response:
         html = response.read()
