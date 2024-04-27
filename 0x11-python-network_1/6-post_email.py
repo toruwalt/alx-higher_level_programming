4905f9295b9444ea182f84fdfe97bfc59f367f0b
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-26 21:35:05 -0700

    Python Network 1 -Task 6:Correction 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,11 +7,8 @@
 if __name__ == '__main__':
     import sys
     import requests
-
     url = sys.argv[1]
-    value = {
-            'email' : sys.argv[2]
-            }
+    value = {'email': sys.argv[2]}
 
     r = requests.post(url, data=value)
     print(r.text)
