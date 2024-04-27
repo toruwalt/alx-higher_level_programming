52edc062b52a445366b90865ee69d242e3145b80
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-27 01:18:12 +0000

    Python Network 1 - Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./2-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
