704dff547e27f8a5219c6be502a3c6175979d04f
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-26 21:40:02 -0700

    Python Network 1 -Task 6:Correction 4

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Script that takes in a URL and an email address, 
-    sends a POST request to the URL with email
-    use requests and sys
-"""
+"""Script that takes in a URL and an email"""
+import sys
+import requests
+
 
 if __name__ == '__main__':
-    import sys
-    import requests
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
-
     r = requests.post(url, data=value)
-    print(r.text)    
+    print(r.text)
