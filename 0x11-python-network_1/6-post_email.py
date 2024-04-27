3b9b85ab4e399e4c5d5ce00e49d120771f43277d
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-26 20:44:55 -0700

    Python Network 1 - Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that takes in a URL and an email address, 
+    sends a POST request to the URL with email
+    use requests and sys
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    data = {
+            'email' : sys.argv[2]
+            }
+
+    r = requests.post(url, data)
+    print(r.text)
+    
