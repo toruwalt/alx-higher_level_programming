b60ff3ed62ebb151117342e39daa2b52b2397ece
Torubiri Walter <toruwalt1997@gmail.com>
2024-04-28 16:09:12 -0700

    Python Network 1 - Task 8:Correction 1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == '__main__':
     try:
         letter = sys.argv[1]
-        lll = {'q':letter}
+        lll = {'q': letter}
         url = 'http://0.0.0.0:5000/search_user'
         r = requests.post(url, data=lll)
         m = r.json()
