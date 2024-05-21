fd7cf27fcc4b958e4ba1138130709481b9766178
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-21 20:15:47 +0000

    JS Web Scrap:Correction 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response) => {
-	if (err){
-		console.error(`code: ${response.statusCode}`);
-	} else {
-		console.log(`code: ${response.statusCode}`);
-	}
+  if (err) {
+    console.error(`code: ${response.statusCode}`);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
