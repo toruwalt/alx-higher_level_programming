052925fe4a61641a2ef6f19731bfb79ef014ecaf
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-21 19:41:32 +0000

    JS Web Scrap - Task 1:Fixed Semi

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,10 +5,8 @@ const file = process.argv[2];
 const text = process.argv[3];
 
 // Read the file asynchronously
-fs.writeFile(file, text, 'utf8', (err, data) => {
+fs.writeFile(file, text, 'utf8', err => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
   }
 });
